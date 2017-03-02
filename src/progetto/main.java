fee02d90480af870f571d5c716ab9f036d17131e
omararafat <arafatomar23@gmail.com>
2017-03-02 20:51:47 +0100

    refactoring

--- src/progetto/main.java
+++ src/progetto/main.java
@@ -11,15 +11,14 @@ public static void main(String[] args){
 	    	x.login();
 	    	int jobID = x.execute(Queries.QUERY1.text());//10 most used programs
 //execute() will set the class internal value to the output of the program if everything goes fine and return the jobID	    	
-   			String input= x.getOutpuBoa(jobID);
    			x.parseXml(new File("C:/progetto tesi/eclipse workspace/progetto/src/progetto/output1.xml") );
-	    	x.parseOutputBoa(input);
+	    	x.parseOutputBoa();
 	    	x.freemarker("templateCsv.ftlh", "csv"+jobID+".csv");
 	    	x.freemarker("templateHTML.ftlh", "html"+jobID+".html");
 
 	    //job churn rate, i need to change the parsing xml;
 	    	jobID = 52384;
-	    	input = x.getOutpuBoa(jobID);
+	    	String input = x.getOutpuBoa(jobID);
 	    	x.parseXml(new File("C:/progetto tesi/eclipse workspace/progetto/src/progetto/output2.xml") );
 	    	x.parseOutputBoa();
 	    	x.freemarker("templateCsv.ftlh", "csv2"+jobID+".csv");
